ca8dc1bb1a8f3067a56abf3244aea8eae206133b
workinfox <feel.fox34@gmail.com>
2025-05-30 21:43:48 +0700

    secont commit in project - function sum_number

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def Hello():
     print("Hello, it's my repozitory for practic")
 
+def sum_number(num1, num2):
+    result = num1 + num2
+    print(f"Sum: {num1} and {num2} equal to {result}")
+    return result
+
 def main():
     Hello()
+    sum_number(25, 5)
 
 if __name__ == '__main__':
     main()
