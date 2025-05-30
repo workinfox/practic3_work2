83da35592a691eef0029b5618fec3ed42f23ab5e
workinfox <feel.fox34@gmail.com>
2025-05-30 21:52:50 +0700

    3 commit in project - function subtraction

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def Hello():
     print("Hello, it's my repozitory for practic")
 
+def subtr(num3, num4):
+    result = num3 - num4
+    print(f"Subtraction {num3} and {num4} equal to: {result}")
+    return result
+
 def main():
     Hello()
+    subtr(30, 5)
 
 if __name__ == '__main__':
     main()
