46bf7c9d64f6a01f71000e7c93f232f7f4c644b6
workinfox <feel.fox34@gmail.com>
2025-05-30 22:07:33 +0700

    5 commit in project - function End

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 def Hello():
     print("Hello, it's my repozitory for practic")
 
-<<<<<<< HEAD
+
 def subtr(num3, num4):
     result = num3 - num4
     print(f"Subtraction {num3} and {num4} equal to: {result}")
-=======
+
 def sum_number(num1, num2):
     result = num1 + num2
     print(f"Sum: {num1} and {num2} equal to {result}")
->>>>>>> function1
     return result
+
+def End():
+    print("Bye-bye, I'm finally done with this")
 
 def main():
     Hello()
-<<<<<<< HEAD
     subtr(30, 5)
-=======
     sum_number(25, 5)
->>>>>>> function1
+    End()
+
 
 if __name__ == '__main__':
     main()
